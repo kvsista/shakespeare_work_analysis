540320a185cbdb5fe38fe6d0780d7aa534120bdb
Kamesh Sista <kvsista@outlook.com>
2019-09-18 20:45:26 -0400

    Deleted function calls

--- scraper.py
+++ scraper.py
@@ -57,12 +57,3 @@ def textfromlink(linklist):
     fulltext_string = ' '.join(fulltext_list)
 
     return fulltext_string
-
-# Let's assign a variable named 'url' to the website we intend to scrape
-url = 'http://shakespeare.mit.edu/'
-
-# Let's extract the home_links and internal_links from the entire website
-home_links, internal_links = page_links(url)
-
-# Let's extract the text from each internal_link and stitch them together into a single string
-full_text = textfromlink(internal_links)
