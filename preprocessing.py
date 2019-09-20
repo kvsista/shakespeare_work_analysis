776a77aa667ad1401852ba7d3223d0ad34802eac
Kamesh Sista <kvsista@outlook.com>
2019-09-19 21:54:18 -0400

    Preprocessing and selenium automation

---
+++ preprocessing.py
@@ -0,0 +1,28 @@
+import nltk
+# nltk.download('all')
+from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
+from nltk.probability import FreqDist
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+def text_data(text):
+
+    text = text.lower()
+
+    word_tokens = word_tokenize(text)
+    sent_tokens = sent_tokenize(text)
+
+    clean_word_tokens = RegexpTokenizer(r'\w+').tokenize(text)
+
+    # Let's filter the stopwords
+    stop_words = set(stopwords.words("english"))
+    filtered_clean_word_tokens = [word for word in clean_word_tokens if word not in stop_words]
+
+    # Let's tag the Parts of Speech
+    tagged_filtered_clean_word_tokens = nltk.pos_tag(filtered_clean_word_tokens)
+
+    # Let's look at the frequency of distribution
+    words_fdist = FreqDist(tagged_filtered_clean_word_tokens)
+
+    return clean_word_tokens, filtered_clean_word_tokens, tagged_filtered_clean_word_tokens, word_tokens, sent_tokens, words_fdist
+
--- shakespeares_work.py
+++ shakespeares_work.py
@@ -1,4 +1,5 @@
 from scraper import page_links, textfromlink
+from preprocessing import text_data
 
 # Let's assign a variable named 'url' to the website we intend to scrape
 url = 'http://shakespeare.mit.edu/'
@@ -8,3 +9,7 @@ home_links, internal_links = page_links(url)
 
 # Let's extract the text from each internal_link and stitch them together into a single string
 full_text = textfromlink(internal_links)
+
+clean_word_tokens, filtered_clean_word_tokens, tagged_filtered_clean_word_tokens, word_tokens, sent_tokens, words_fdist = text_data(full_text)
+
+
